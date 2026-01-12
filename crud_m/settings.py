20b53dbd3e1b1180623f206bb36ac0dc96878b2b
albandev <albandev>
2026-01-12 15:09:53 -0300

    creado hoja de estilos

--- crud_m/settings.py
+++ crud_m/settings.py
@@ -115,4 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Directorios donde Django buscará archivos estáticos
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Tu carpeta static global
+    # BASE_DIR / 'tu_aplicacion' / 'static',  # Si quieres static por app
+]
